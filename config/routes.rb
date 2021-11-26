bbc2f615f0ede6152529fb4b533b4b85ed83223c
kazukazu727 <kazuma.xdbxdbx@gmail.com>
2021-11-26 12:07:53 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :admins
   root to: "homes#top"
   get "home/about" =>"homes#about"
+  resources :genres, only: [:index, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
