dd49da4c0555e6431be010e1aec5ae0e01d9d5c9
kazukazu727 <kazuma.xdbxdbx@gmail.com>
2021-12-08 14:58:56 +0000

    Add3

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -10,16 +10,24 @@ class Admin::GenresController < ApplicationController
     @genres=Genre.all
     if @genre.save
       flash[:notice] = "Genre was successfully created."
-      redirect_to genre_path(@genre.id)
+      redirect_to admin_genres_path
     else
       render :index
     end
   end
 
   def edit
+    @genre=Genre.find(params[:id])
   end
 
   def update
+    @genre=Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice]="genre was successfully update"
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
   end
 
   private
