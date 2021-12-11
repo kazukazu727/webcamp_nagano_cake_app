59f0a6b6284e248f47ffc600aa26e4e594e0e927
kazukazu727 <kazuma.xdbxdbx@gmail.com>
2021-12-11 16:50:27 +0000

    Add4

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,21 +1,21 @@
 class Admin::ItemsController < ApplicationController
-  
-  def index
-  end
-  
+
   def new
   end
-  
+
+  def index
+  end
+
   def create
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
 end
