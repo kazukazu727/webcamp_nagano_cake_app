5320737e679f0b04501733336090128a8016c5a8
kazukazu727 <kazuma.xdbxdbx@gmail.com>
2021-12-15 15:16:36 +0000

    Add7

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,7 +7,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item=Item.new(item_params)
     @item.save
-    redirect_to items_path
+    redirect_to admin_items_path
   end
 
   def index
