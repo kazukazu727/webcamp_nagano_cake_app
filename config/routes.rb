f358e35a836894441b6f8b656f00464483b5c59f
kazukazu727 <kazuma.xdbxdbx@gmail.com>
2022-01-16 14:47:01 +0000

    [Add11]

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :items, only:[:index, :new, :create, :show, :edit, :update]
   end
+
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
