9829d1993974c00c99f07e9ae6e86d77d6fd64d2
kazukazu727 <kazuma.xdbxdbx@gmail.com>
2022-01-30 17:34:29 +0000

    [Add19]

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,15 +8,40 @@ class CustomersController < ApplicationController
   end
 
   def edit
+    @customer=Customer.find(params[:id])
+		if @customer.id != current_customer.id
+			redirect_to root_path
+		end
   end
 
   def update
+    @customer = Customer.find(params[:id])
+		if @customer.update(customer_params)
+			if customer_signed_in?
+				flash[:notice] = "登録情報が更新されました。"
+				redirect_to customer_path(current_customer)
+			else
+				redirect_to request.referrer
+			end
+		else
+			flash[:notice] = "項目を正しく記入してください"
+			redirect_to request.referrer
+		end
   end
 
   def unsubscribe
+    @customer = Customer.find(params[:id])
+		if @customer.id != current_customer.id
+			redirect_to root_path
+		end
   end
 
   def withdrawal
+    @customer = Customer.find(params[:id])
+		@customer.update(customer_params)
+		reset_session
+		flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
+		redirect_to root_path
   end
 
   private
